68f8ff52c8bdefc38f189d3789843aef68d21937
sydney miranda <sydmiranda@icloud.com>
2020-10-26 16:38:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,5 +10,5 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-var newArray = [... array, element]
+var newArray = [...array, element]
 }
