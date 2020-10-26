e5478b671bcfa5c0c968bb024a9e6b3f3fe6729d
sydney miranda <sydmiranda@icloud.com>
2020-10-26 16:36:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,10 @@
 
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToEndOfArray(array, element) {
+  
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element)
+}
